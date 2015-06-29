193d291138a024c0249251fa3ea871bff66fcb5b
Loren Hale <loren.hale@gmail.com>
2015-06-29 12:37:30 +0800

    add acts_as_taggable_on

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -69,6 +69,6 @@ class AgenciesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def agency_params
-      params.require(:agency).permit(:name, :description)
+      params.require(:agency).permit(:name, :description, :grade, :tag_list)
     end
 end
--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,2 +1,3 @@
 class Agency < ActiveRecord::Base
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
 end
