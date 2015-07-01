7fad506db389bc64910d6030abc972405d129368
Loren Hale <loren.hale@gmail.com>
2015-07-01 14:49:16 +0800

    add tagFilter

---
+++ app/assets/javascripts/ng-app/filters/tagFilter.js
@@ -0,0 +1,27 @@
+angular.module('app').filter('tagsFilter', function () {
+  return function (items, tags) {
+    var filtered = [];
+    var matches = false;
+    if (tags) {
+      tags = tags || '';
+      tags = tags.match(/[^ ,]+/g);
+
+      (items || []).forEach(function (item) {
+        var matches = tags.every(function (tag) {
+          var mapping = item.tag_list.map(function(tag_name){
+            return (tag_name.indexOf(tag) > -1);
+          });
+          if ($.inArray(true, mapping) > -1) {
+            return true;
+          }
+        });
+        if (matches) {
+          filtered.push(item);
+        }
+      });
+      return filtered;
+    } else {
+      return items;
+    }
+  };
+});
