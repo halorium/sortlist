dd389e9a7d6868950df1dcda2c2fc2357e558bec
Loren Hale <loren.hale@gmail.com>
2015-07-01 19:06:48 +0800

    refactor seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,28 @@
 
 agencies = Agency.create(
   [
-    { name: 'Acme',         description: 'Good Agency',    grade: 'padawan', tag_list: 'placement, recruiting' },
-    { name: 'Good People',  description: 'Top One',        grade: 'jedi',    tag_list: 'placement, recruiting, contracting' },
-    { name: 'Rock Results', description: 'We get it done', grade: 'master',  tag_list: 'contracting, hiring' }
+    { name: 'Havas Worldwide',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      grade: 'master',
+      tag_list: 'Full Integrated, Advertising'
+    },
+
+    { name: 'Huge New York',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      grade: 'master',
+      tag_list: 'Digital Advertising, User Experience'
+    },
+
+    { name: 'Grey Group',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      grade: 'jedi',
+      tag_list: 'Brand Activation, Public Relations'
+    },
+
+    { name: 'Mindshare',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      grade: 'padawan',
+      tag_list: 'Media Planning, Advertising'
+    }
   ]
 )
