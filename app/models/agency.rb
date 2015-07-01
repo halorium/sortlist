6e1ece48a69a276b468dbcd0bd71e0032247b6bb
Loren Hale <loren.hale@gmail.com>
2015-07-01 14:49:52 +0800

    add agency scopes

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,3 +1,6 @@
 class Agency < ActiveRecord::Base
   acts_as_taggable # Alias for acts_as_taggable_on :tags
+
+  scope :by_name, ->(name) { where('name LIKE ?', "%#{name}%") }
+  scope :by_tags, ->(tags) { joins(:taggings).joins(:tags).where('tags.name LIKE ?', "%#{tags}%") }
 end
