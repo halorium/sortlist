b542961fb5034d6ca9a9a818f84bbc7c1066b5ad
Loren Hale <loren.hale@gmail.com>
2015-07-02 17:53:42 +0800

    add model validations

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,6 +1,9 @@
 class Agency < ActiveRecord::Base
   acts_as_taggable # Alias for acts_as_taggable_on :tags
 
+  validates :name, :grade, presence: true
+  validates :grade, inclusion: { in: %w(padawan jedi master), message: "%{value} is not a valid grade" }
+
   scope :by_name, ->(name) { where('name LIKE ?', "%#{name}%") }
   scope :by_tags, ->(tags) { joins(:taggings).joins(:tags).where('tags.name LIKE ?', "%#{tags}%") }
 end
